a4687c72adfd0ca01539918dcb05f46b788e397f
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2026-05-16 10:59:08 -0300

    server chequear login

--- servidor.py
+++ servidor.py
@@ -1,4 +1,15 @@
 import socket
+import sqlite3
+
+def verificar_login(usuario, contraseña):
+    conn_db=sqlite3.connect("miBBDD.db")
+    cursor=conn_db.cursor()
+    cursor.execute("SELECT*FROM usuarios WHERE usuario)=? AND contraseña=?", (usuario, contraseña)
+    )
+    resultado=cursor.fetchone()
+    conn_db.close()
+    return resultado is not None
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -18,7 +29,9 @@ while True:
     if comando == "login":
         usuario = partes[1]
         clave = partes[2]
-        conn.send(f"Procesando login de {usuario}".encode())
+        if verificar_login(usuario,contraseña):
+            conn.send("Login exitoso".encode())
+        else: conn.send("Usuario o contraseña incorrectos".encode())
     elif comando == "registro":
         usuario = partes[1]
         conn.send(f"Registrando a {usuario}".encode())
